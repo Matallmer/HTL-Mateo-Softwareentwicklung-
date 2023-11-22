9e7c4bbd3f23b6fe8657c37067aeb7f5b8db2613
Mateo <ALL230088@spengergasse.at>
2023-11-22 20:36:32 +0100

    ...

---
+++ 16-11-23/java.js
@@ -0,0 +1,9 @@
+// Dassind die Befehle die ich bis jetzt gelernt:
+document.getElementById("id name");
+alert("Hallo Welt");
+prompt("Bitte gib hier eine Zahl ein", "Hier Zahl");
+// Zahl=number , Text=String, Wahrheitswert=boelean
+// number+number=number, number+String=String, boelean+number=number
+const x = 12;
+document.getElementById("id name").innerHTML = x;
+typeof x;
