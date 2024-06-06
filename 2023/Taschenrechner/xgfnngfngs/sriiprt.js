69f5f0e6893d47b96fde3b1ce614e39133f1f4c1
Mateo Allmer <ALL230088@spengergasse.at>
2024-06-06 12:53:34 +0200

    ...

--- 2023/Taschenrechner/xgfnngfngs/sriiprt.js
+++
@@ -1,9 +0,0 @@
-var x = 55;
-
-function addieren(zahl, sieben) {
-  return zahl + sieben;
-}
-var summe = addieren(x, 7);
-
-document.getElementById("ergebnisParagraph").innerHTML =
-  "Das Ergebnis der Berechnung ist: " + summe;
